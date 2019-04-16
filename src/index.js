c380c4d184d54c95daf3f4d2314dd4696d9d355c
Your Name <jack.sullivan96@gmail.com>
2019-04-16 00:11:32 -0400

    app

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import DiscoverPage from "./DiscoverPage";
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<DiscoverPage />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
